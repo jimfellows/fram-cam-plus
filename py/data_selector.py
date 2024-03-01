ec0d017f71f7589ba94af7392bb2a081bd51fe6c
jimfellows <jim.fellows01@gmail.com>
2024-03-01 14:24:52 -0800

    checkpoint, partway through proxy filter model revamp

--- py/data_selector.py
+++ py/data_selector.py
@@ -3,6 +3,7 @@
 from PySide6.QtSql import QSqlQueryModel, QSqlRelationalTableModel, QSqlRelation, QSqlQuery, QSqlRecord
 from PySide6.QtCore import QObject, PyClassProperty, Property, Slot, Signal, QSortFilterProxyModel
 from py.logger import Logger
+from py.qt_models import FramCamSqlListModel, FramCamFilterableModel, HaulsModel, CatchesModel, ProjectsModel, BiosModel
 
 
 class FramCamQueryModel(QSqlQueryModel):
@@ -67,41 +68,11 @@ class FramCamQueryModel(QSqlQueryModel):
         return self.record(i).value(col_name)
 
 
-class HaulsModel(FramCamQueryModel):
+class HaulsModelV1(FramCamSqlListModel):
     def __init__(self, db):
         super().__init__(db)
-        self._logger = Logger.get_root()
-        self._sql = '''
-            select
-                        haul_number
-                        ,haul_id
-                        ,vessel_name
-                        ,station_code
-                        ,latitude_min
-                        ,latitude_max
-                        ,longitude_min
-                        ,longitude_max
-            from        hauls
-            order by    cast(haul_number as bigint) desc
-        '''
-        self.populate()
-
-    @Slot(name='populate')
-    def populate(self):
-        self.clear()
-        self._query.prepare(self._sql)
-        self._query.exec()
-        self.setQuery(self._query)
-        self.model_loaded.emit()
-
-    @property
-    def cur_haul_number(self):
-        return self.getRowValue(self._current_index, 'HAUL_NUMBER')
-
-    @property
-    def cur_haul_id(self):
-        return self.getRowValue(self._current_index, 'HAUL_ID')
-
+        self.sql = 'select * from fram_cam_hauls order by cast(haul_number as bigint) desc'
+        self.loadModel()
 
 class CatchOptionsModel(FramCamQueryModel):
     def __init__(self, db):
@@ -247,42 +218,40 @@ class DataSelector(QObject):
         # setup hauls model
         # TODO: proxy models for in memory sorting?
         self._hauls_model = HaulsModel(db)
-        self._catches_model = CatchOptionsModel(db)
-        self._projects_model = ProjectOptionsModel(db)
-        self._bios_model = BioOptionsModel(db)
-
-        # vars to hold rec qml model item that is currently selected
-        self._cur_haul_rec = None
-        self._cur_catch_rec = None
-        self._cur_project_rec = None
-        self._cur_bio_label_rec = None
+        self._hauls_model.loadModel()
+        self._catches_model = CatchesModel(db)
+        self._catches_proxy = FramCamFilterableModel(self._catches_model)
+        self._projects_model = ProjectsModel(db)
+        self._projects_proxy = FramCamFilterableModel(self._projects_model)
+        self._bios_model = BiosModel(db)
+        self._bios_proxy = FramCamFilterableModel(self._bios_model)
 
         # when haul changes populate the other models
-        self._hauls_model.current_index_changed.connect(lambda i: self._on_haul_changed(i))
-        self._catches_model.current_index_changed.connect(lambda i: self._on_catch_changed(i))
-        self._projects_model.current_index_changed.connect(lambda i: self._on_project_changed(i))
-        self._bios_model.current_index_changed.connect(lambda i: self._on_bio_changed(i))
+        self._hauls_model.currentIndexChanged.connect(lambda i: self._on_haul_changed(i))
+        self._catches_model.currentIndexChanged.connect(lambda i: self._on_catch_changed(i))
+        self._projects_model.currentIndexChanged.connect(lambda i: self._on_project_changed(i))
+        self._bios_model.currentIndexChanged.connect(lambda i: self._on_bio_changed(i))
 
         """
         Below we select model rows pulled from database on startup.  Note that we set _current_index, not
         current_index to avoid signaling to UI directly.  We use this backchannel because...
         """
         # if we have pre-selected vals from db, set them now, in order (haul,catch,project,bio)
-        _haul_model_ix = self._hauls_model.get_ix_by_value('HAUL_ID', self._app.state.cur_haul_id)
-        self._hauls_model._current_index = _haul_model_ix
-        self._on_haul_changed(_haul_model_ix)
+        # _haul_model_ix = self._hauls_model.get_ix_by_value('HAUL_ID', self._app.state.cur_haul_id)
+        # self._hauls_model._current_index = _haul_model_ix
+        # self._on_haul_changed(_haul_model_ix)
 
-        _catch_model_ix = self._catches_model.get_ix_by_value('CATCH_ID', self._app.state.cur_catch_id)
-        self._catches_model._current_index = _catch_model_ix
-        self._on_catch_changed(_catch_model_ix)
-
-        _projects_model_ix = self._projects_model.get_ix_by_value('PROJECT', self._app.state.cur_project)
-        self._projects_model._current_index = _projects_model_ix
-        self._on_project_changed(_projects_model_ix)
-
-        _bios_model_ix = self._bios_model.get_ix_by_value('BIO_LABEL', self._app.state.cur_bio_label)
-        self._bios_model._current_index = _bios_model_ix
-        self._on_bio_changed(_bios_model_ix)
+        # _catch_model_ix = self._catches_model.get_ix_by_value('CATCH_ID', self._app.state.cur_catch_id)
+        # self._catches_model._current_index = _catch_model_ix
+        # self._on_catch_changed(_catch_model_ix)
+        #
+        # _projects_model_ix = self._projects_model.get_ix_by_value('PROJECT', self._app.state.cur_project)
+        # self._projects_model._current_index = _projects_model_ix
+        # self._on_project_changed(_projects_model_ix)
+        #
+        # _bios_model_ix = self._bios_model.get_ix_by_value('BIO_LABEL', self._app.state.cur_bio_label)
+        # self._bios_model._current_index = _bios_model_ix
+        # self._on_bio_changed(_bios_model_ix)
 
 
     def _get_haul_ix_by_id_v2(self, haul_id):
@@ -302,16 +271,15 @@ class DataSelector(QObject):
             proxy_ix = self._hauls_proxy_model.index(0, haul_field_ix)
             return self._hauls_proxy_model.map_to_source(proxy_ix)[0]
 
-
     def _on_haul_changed(self, new_haul_index):
+        print(f"New haul changed to {new_haul_index}")
         # TODO: set cur haul id here?
-        self._cur_haul_rec = self._hauls_model.record(new_haul_index)
-        # self._logger.info(f"Selected haul id changed to {self._cur_haul_rec}")
-        self._catches_model.populate(self._cur_haul_rec.value('HAUL_ID'))
-        self._projects_model.clear()
-        self._bios_model.clear()
-        self._app.state.set_state_value('Current Haul ID', self._cur_haul_rec.value('HAUL_ID'))
-        self._app.state.set_state_value('Current Haul Number', self._cur_haul_rec.value('HAUL_NUMBER'))
+        self._cur_haul_rec = self._hauls_model.getItem(new_haul_index)
+        self._catches_model.setBindParam(':fram_cam_haul_id', self._cur_haul_rec['fram_cam_haul_id'])
+        self._catches_model.setBindParam(':fram_cam_haul_id', self._cur_haul_rec['fram_cam_haul_id'])
+        self._catches_model.loadModel()
+        self._projects_model.clearModel()
+        self._bios_model.clearModel()
 
     def _on_catch_changed(self, new_catch_index):
         """
@@ -319,24 +287,28 @@ class DataSelector(QObject):
         :param new_catch_index: new model index
         :return:
         """
-        self._cur_catch_id = self._catches_model.getRowValue(new_catch_index, 'CATCH_ID')
-        self._cur_catch = self._catches_model.record(new_catch_index)
-        self._logger.info(f"Selected catch changed to {self._cur_catch_id}")
-        self._projects_model.populate(self._cur_catch_id)
-        self._bios_model.populate(self._cur_catch_id)
-        self._app.state.set_state_value('Current Catch ID', self._cur_catch_id)
-        self._app.state.set_state_value('Current Catch Display', self._catches_model.record(new_catch_index).value('DISPLAY_NAME'))
+        self._cur_catch_rec = self._catches_model.getItem(new_catch_index)
+        # self._logger.info(f"Selected catch changed to {self._cur_catch_id}")
+        self._projects_model.setBindParam(':fram_cam_haul_id', self._cur_catch_rec['fram_cam_haul_id'])
+        self._projects_model.loadModel()
+        self._projects_proxy.filterOnStrRole('display_name', self._cur_catch_rec['display_name'])
+        self._bios_model.setBindParam(':fram_cam_haul_id', self._cur_catch_rec['fram_cam_haul_id'])
+        self._bios_model.loadModel()
+        self._bios_proxy.filterOnStrRole('display_name', self._cur_catch_rec['display_name'])
+
+        self._app.state.set_state_value('Current Catch ID', self._cur_catch_rec['fram_cam_catch_id'])
+        self._app.state.set_state_value('Current Catch Display', self._cur_catch_rec['display_name'])
 
     def _on_project_changed(self, new_project_index):
-        self._cur_project_name = self._projects_model.getRowValue(new_project_index, 'PROJECT')
-        self._logger.info(f"Selected project changed to {self._cur_project_name}")
-        self._bios_model.populate(self._cur_catch_id, self._cur_project_name)
-        self._app.state.set_state_value('Current Project', self._cur_project_name)
+        self._cur_project_rec = self._projects_model.getItem(new_project_index)
+        # self._logger.info(f"Selected project changed to {self._cur_project_name}")
+        self._bios_proxy.filterOnStrRole('project_name', self._cur_project_rec['project_name'])
+        self._app.state.set_state_value('Current Project', self._cur_project_rec['project_name'])
 
     def _on_bio_changed(self, new_bio_index):
-        self._cur_bio_label = self._bios_model.getRowValue(new_bio_index, 'BIO_LABEL')
-        self._logger.info(f"Selected bio label changed to {self._cur_bio_label}")
-        self._app.state.set_state_value('Current Bio Label', self._cur_bio_label)
+        self._cur_bio_rec = self._bios_model.getItem(new_bio_index)
+        # self._logger.info(f"Selected bio label changed to {self._cur_bio_label}")
+        self._app.state.set_state_value('Current Bio Label', self._cur_bio_rec['bio_label'])
 
     @Property(QObject, notify=unusedSignal)
     def hauls_model(self):
@@ -350,12 +322,24 @@ class DataSelector(QObject):
         return self._catches_model
 
     @Property(QObject, notify=unusedSignal)
+    def catches_proxy(self):
+        return self._catches_proxy
+
+    @Property(QObject, notify=unusedSignal)
     def projects_model(self):
         return self._projects_model
 
     @Property(QObject, notify=unusedSignal)
+    def projects_proxy(self):
+        return self._projects_proxy
+
+    @Property(QObject, notify=unusedSignal)
     def bios_model(self):
         return self._bios_model
+
+    @Property(QObject, notify=unusedSignal)
+    def bios_proxy(self):
+        return self._bios_proxy
 
     # def _set_cur_haul_number(self):
     #     self._current_haul_number = self._hauls_model.getRowValue(self._h)
