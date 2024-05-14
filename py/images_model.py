712660186545ccbf2bed8c62b7ac1270c6e6ce27
Jim Fellows <james.fellows@noaa.gov>
2024-05-13 21:05:45 -0700

    clean up

--- py/images_model.py
+++ py/images_model.py
@@ -121,10 +121,8 @@ class ImagesModel(FramCamSqlListModel):
 
         self._logger.debug(f"SETTING {role_name}={value} for image {self.curImgId}")
         self.setData(self._selected_index, value, role_name)
-        self._logger.error(f"Our imageid = {self.curImgId}")
         for _i in range(self._table_model.rowCount()):  # todo: is this iteration the best way to do this?
             if self._table_model.record(_i).value('image_id') == self.curImgId:
-                print(f"UPDATING RECORD with image id: {self.curImgId}")
                 _r = self._table_model.record(_i)
                 _r.setValue(self._table_model.fieldIndex(role_name.upper()), value)
                 self._table_model.setRecord(_i, _r)
